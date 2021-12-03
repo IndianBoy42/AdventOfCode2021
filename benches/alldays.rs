f5bc8171e0de085df6a190ffa6e392e3b519751c
Anshuman <amedhi@connect.ust.hk>
2021-12-03 14:41:08 +0800

    clean and optimize

--- benches/alldays.rs
+++ benches/alldays.rs
@@ -16,12 +16,16 @@ pub fn criterion_benchmark(crit: &mut Criterion) {
     c.sample_size(100);
 
     let input1 = utils::read_input("input1.txt").unwrap();
-    c.bench_function("d1p1", |b| b.iter(|| day1::input(black_box(&input1))));
+    c.bench_function("d1p1", |b| b.iter(|| day1::part2(black_box(&input1))));
     c.bench_function("d1p2", |b| b.iter(|| day1::part2(black_box(&input1))));
 
-    let input1 = utils::read_input("input2.txt").unwrap();
-    c.bench_function("d2p1", |b| b.iter(|| day2::part1(black_box(&input1))));
-    c.bench_function("d2p2", |b| b.iter(|| day2::input(black_box(&input1))));
+    let input2 = utils::read_input("input2.txt").unwrap();
+    c.bench_function("d2p1", |b| b.iter(|| day2::part1(black_box(&input2))));
+    c.bench_function("d2p2", |b| b.iter(|| day2::part2(black_box(&input2))));
+
+    let input3 = utils::read_input("input3.txt").unwrap();
+    c.bench_function("d3p1", |b| b.iter(|| day3::part1(black_box(&input3))));
+    c.bench_function("d3p2", |b| b.iter(|| day3::part2(black_box(&input3))));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/day3.rs
+++ src/day3.rs
@@ -1,9 +1,10 @@
-use std::str::from_utf8;
+use std::{ops::ControlFlow, str::from_utf8};
 
 use crate::utils::*;
 
 pub fn part1(input: &str) -> usize {
-    let counts = input.lines().fold([(0, 0); 12], |mut acc, line| {
+    let n = input.lines().clone().next().unwrap().len();
+    let counts = input.lines().fold(vec![(0, 0); n], |mut acc, line| {
         line.bytes()
             .zip(&mut acc)
             .for_each(|(byte, (zeros, ones))| {
@@ -15,66 +16,74 @@ pub fn part1(input: &str) -> usize {
             });
         acc
     });
-    let (gamma, eps) = counts
-        .into_iter()
-        .fold((0, 0), |(gamma, eps), (zeros, ones)| {
-            (
-                gamma << 1 | (if zeros < ones { 1 } else { 0 }),
-                eps << 1 | (if zeros < ones { 0 } else { 1 }),
-            )
-        });
+
+    // let (gamma, eps) = counts
+    // .into_iter()
+    // .fold((0, 0), |(gamma, eps), (zeros, ones)| {
+    // let bit = (zeros < ones) as usize;
+    // (gamma << 1 | bit, eps << 1 | (1 - bit))
+    // });
+    let gamma = counts.into_iter().fold(0, |gamma, (zeros, ones)| {
+        let bit = (zeros < ones) as usize;
+        gamma << 1 | bit
+    });
+    let eps = !gamma & !(!0 << n);
+
     eps * gamma
 }
 
 pub fn part2(input: &str) -> usize {
-    let lines = input.lines();
-    let n = lines.clone().next().unwrap().len();
+    let lines = input.lines().map(str::as_bytes).collect_vec();
 
-    let mut ox = lines.clone().map(str::as_bytes).collect_vec();
-    for i in 0..12 {
-        let ones = ox.iter().map(|x| x[i]).filter(|&x| x == b'1').count();
-        let gamma = if ones >= (ox.len() - ones) {
-            b'1'
-        } else {
-            b'0'
-        };
-        ox = ox.into_iter().filter(|s| s[i] == gamma).collect_vec();
+    let ox = lines.clone();
+    let ox = to_number(filter(ox, |s, gamma| s == gamma).iter().copied());
+
+    let co = lines;
+    let co = to_number(filter(co, |s, gamma| s != gamma).iter().copied());
+
+    ox * co
+}
+
+fn to_number(ox: impl IntoIterator<Item = u8>) -> usize {
+    ox.into_iter()
+        .fold(0, |acc, bit| acc << 1 | (bit == b'1') as usize)
+}
+
+fn filter<F>(mut ox: Vec<&[u8]>, f: F) -> &[u8]
+where
+    F: Fn(u8, u8) -> bool,
+{
+    for i in 0.. {
+        // Count most common bit
+        ox = filter_once(i, ox, &f);
         if ox.len() == 1 {
             break;
         } else if ox.is_empty() {
             unreachable!();
         }
     }
+    ox.first().unwrap()
+}
 
-    let mut co = lines.map(str::as_bytes).collect_vec();
-    for i in 0..12 {
-        let ones = co.iter().map(|x| x[i]).filter(|&x| x == b'1').count();
-        let gamma = if ones >= (co.len() - ones) {
-            b'1'
-        } else {
-            b'0'
-        };
-        co = co.into_iter().filter(|s| s[i] != gamma).collect_vec();
-        if co.len() == 1 {
-            break;
-        } else if co.is_empty() {
-            unreachable!();
-        }
-    }
-
-    let ox = ox.first().unwrap().iter().fold(0, |gamma, &bit| {
-        gamma << 1 | (if bit == b'1' { 1 } else { 0 })
-    });
-    let co = co.first().unwrap().iter().fold(0, |gamma, &bit| {
-        gamma << 1 | (if bit == b'1' { 1 } else { 0 })
-    });
-
-    ox * co
+fn filter_once<F>(i: usize, ox: Vec<&[u8]>, f: F) -> Vec<&[u8]>
+where
+    F: Fn(u8, u8) -> bool,
+{
+    // let ones = ox.iter().map(|x| x[i]).filter(|&x| x == b'1').count();
+    let ones = ox
+        .iter()
+        .map(|x| unsafe { *x.get_unchecked(i) })
+        .filter(|&x| x == b'1')
+        .count();
+    let most = (ones >= (ox.len() - ones)) as u8 + b'0';
+    ox.into_iter().filter(|s| f(s[i], most)).collect_vec()
+    // ox.retain(|s| f(s[i], most)); // So much slower!?
+    // ox
 }
 
 #[test]
 fn test() {
-    let input = read_input("input").unwrap();
+    let input = read_input("input3.txt").unwrap();
     assert_eq!(part1(&input), 2_498_354);
     assert_eq!(part2(&input), 3_277_956);
 }
