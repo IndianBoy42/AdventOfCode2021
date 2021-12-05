6a02a7cf9bf76c00dd739b3f5b2a1a3fd3901fea
Anshuman <amedhi@connect.ust.hk>
2021-12-05 14:03:22 +0800

    clean

--- src/day5.rs
+++ src/day5.rs
@@ -3,81 +3,67 @@ use std::{mem::size_of_val, ops::ControlFlow, str::from_utf8};
 use crate::utils::*;
 
 // TODO: maybe use complex numbers can make the iterations cleaner?
+// TODO: use a contiguous grid instead of map
 
-fn parse(input: &str) -> impl Iterator<Item = ((u16, u16), (u16, u16))> + '_ {
+type Key = u16;
+
+#[inline(always)]
+fn parse(input: &str) -> impl Iterator<Item = ((Key, Key), (Key, Key))> + '_ {
     input
         .lines()
         .map(|line| line.split_once(" -> ").unwrap())
         .map(|(from, to)| (from.split_once(',').unwrap(), to.split_once(',').unwrap()))
         .map(|((fromx, fromy), (tox, toy))| {
             (
-                (fromx.parse::<u16>().unwrap(), fromy.parse::<u16>().unwrap()),
-                (tox.parse::<u16>().unwrap(), toy.parse::<u16>().unwrap()),
+                (fromx.parse().unwrap(), fromy.parse().unwrap()),
+                (tox.parse().unwrap(), toy.parse().unwrap()),
             )
         })
 }
 
-fn incr(x: u16, y: u16, tiles: &mut FMap<(u16, u16), u8>) {
+#[inline(always)]
+fn incr(x: Key, y: Key, tiles: &mut FMap<(Key, Key), u8>) {
     tiles.entry((x, y)).and_modify(|c| *c += 1).or_insert(1);
 }
 
-fn swap_ordered(from: u16, to: u16) -> (u16, u16) {
+#[inline(always)]
+fn swap_ordered(from: Key, to: Key) -> (Key, Key) {
     (from.min(to), from.max(to))
 }
 
+#[inline(always)]
+fn straight_line(
+    tiles: &mut FMap<(Key, Key), u8>,
+    line @ ((fromx, fromy), (tox, toy)): ((Key, Key), (Key, Key)),
+) {
+    if fromx == tox {
+        let (fromy, toy) = swap_ordered(fromy, toy);
+        for y in fromy..=toy {
+            incr(fromx, y, tiles);
+        }
+    } else if fromy == toy {
+        let (fromx, tox) = swap_ordered(fromx, tox);
+        for x in fromx..=tox {
+            incr(x, fromy, tiles);
+        }
+    }
+}
+
 pub fn part1(input: &str) -> usize {
-    let tiles = parse(input).fold(
-        fmap(5000),
-        |mut tiles, line @ ((fromx, fromy), (tox, toy))| {
-            if fromx == tox {
-                let (fromy, toy) = swap_ordered(fromy, toy);
-                for y in fromy..=toy {
-                    incr(fromx, y, &mut tiles);
-                }
-            } else if fromy == toy {
-                let (fromx, tox) = swap_ordered(fromx, tox);
-                for x in fromx..=tox {
-                    incr(x, fromy, &mut tiles);
-                }
-            }
-            tiles
-        },
-    );
+    let tiles = parse(input).fold(fmap(5000), |mut tiles, line| {
+        straight_line(&mut tiles, line);
+        tiles
+    });
 
     tiles.values().filter(|&&x| x > 1).count()
 }
-
-// fn diag_line(
-//     line @ ((fromx, fromy), (tox, toy)): ((u16, u16), (u16, u16)),
-// ) -> impl Iterator<Item = (u16, u16)> {
-//     let line @ ((fromx, fromy), (tox, toy)) = if fromx > tox {
-//         ((tox, toy), (fromx, fromy))
-//     } else {
-//         line
-//     };
-//
-//     if fromy > toy {
-//         izip!(fromx..=tox, (toy..=fromy).rev())
-//     } else {
-//         izip!(fromx..=tox, fromy..=toy)
-//     }
-// }
 
 pub fn part2(input: &str) -> usize {
     let tiles = parse(input).fold(
         fmap(5000),
         |mut tiles, line @ ((fromx, fromy), (tox, toy))| {
-            if fromx == tox {
-                let (fromy, toy) = (fromy.min(toy), fromy.max(toy));
-                for y in fromy..=toy {
-                    incr(fromx, y, &mut tiles);
-                }
-            } else if fromy == toy {
-                let (fromx, tox) = (fromx.min(tox), fromx.max(tox));
-                for x in fromx..=tox {
-                    incr(x, fromy, &mut tiles);
-                }
-            } else {
+            straight_line(&mut tiles, line);
+            if fromx != tox && fromy != toy {
                 let line @ ((fromx, fromy), (tox, toy)) = if fromx > tox {
                     ((tox, toy), (fromx, fromy))
                 } else {
@@ -85,11 +71,11 @@ pub fn part2(input: &str) -> usize {
                 };
                 if fromy > toy {
                     for (x, y) in izip!(fromx..=tox, (toy..=fromy).rev()) {
-                        tiles.entry((x, y)).and_modify(|c| *c += 1).or_insert(1);
+                        incr(x, y, &mut tiles);
                     }
                 } else {
                     for (x, y) in izip!(fromx..=tox, fromy..=toy) {
-                        tiles.entry((x, y)).and_modify(|c| *c += 1).or_insert(1);
+                        incr(x, y, &mut tiles);
                     }
                 }
             }
