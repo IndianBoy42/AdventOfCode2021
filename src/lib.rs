332cb169d8c0bd2a553111ffe883ef8f08b2c242
Anshuman <amedhi@connect.ust.hk>
2021-12-06 00:24:05 +0800

    day5

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
 
 pub mod grid;
 pub mod searcher;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use aoc21::*;
 
 fn main() {
-    let input1 = utils::read_input("input1.txt").unwrap();
-    dbg!(day1::part1(&input1));
-    dbg!(day1::part2(&input1));
+    let input5 = utils::read_input("input5.txt").unwrap();
+    dbg!(day5::part1(&input5));
+    dbg!(day5::part2(&input5));
 }
