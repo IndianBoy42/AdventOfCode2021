61ac80f2a84b50c3af45fb3c850f8af52b9bb825
Anshuman <amedhi@connect.ust.hk>
2021-12-06 15:40:17 +0800

    exponentiation by squaring for log time

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,52 @@
+use std::mem::size_of_val;
+
+use ndarray::{Array1, Array2};
+
 use crate::utils::*;
 
 const NEWBORN: usize = 8;
 const REFRESH: usize = 6;
 
+fn solve_log(input: &str, days: usize) -> usize {
+    let mut fishes = input
+        .trim()
+        .split(',')
+        .map(str::parse::<u8>)
+        .map(Result::unwrap)
+        .fold(ArrayVec::from([0usize; NEWBORN + 1]), |mut acc, num| {
+            acc[num as usize] += 1;
+            acc
+        });
+
+    let steps = successors(Some(2), |x| Some(x * 2)).take_while(|&x| x <= days);
+    let first = ndarray::array![
+        [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    let mats = {
+        let mut mats: Vec<Array2<usize>> = steps
+            .scan(first.clone(), |mat, _| {
+                *mat = mat.dot(mat);
+                Some(mat.clone())
+            })
+            .collect_vec();
+        mats.insert(0, first);
+        dbg!(mats)
+    };
+
+    let mat = (0..(size_of_val(&days) * 8))
+        .filter(|bitn| days & (1 << bitn) != 0)
+        .map(|x| dbg!(x))
+        .fold(Array2::eye(NEWBORN + 1), |mat, i| mat.dot(&mats[i]));
+    mat.dot(&fishes.into_iter().collect::<Array1<_>>()).sum()
+}
 fn solve(input: &str, days: usize) -> usize {
     let mut fishes = input
         .trim()
@@ -15,21 +59,20 @@ fn solve(input: &str, days: usize) -> usize {
         });
 
     for i in 0..days {
-        dbg!(&fishes);
         let new = fishes.remove(0);
-        fishes[REFRESH] += new;
         fishes.push(new);
+        fishes[REFRESH] += new;
     }
 
     fishes.into_iter().sum()
 }
 
 pub fn part1(input: &str) -> usize {
-    solve(input, 80)
+    solve_log(input, 80)
 }
 
 pub fn part2(input: &str) -> usize {
-    solve(input, 256)
+    solve_log(input, 256)
 }
 
 #[test]
