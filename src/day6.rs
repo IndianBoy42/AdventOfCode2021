7913f721f64544d09d791a463f453b00c4d882d1
Anshuman <amedhi@connect.ust.hk>
2021-12-06 15:53:33 +0800

    cleaner log time solution

--- src/day6.rs
+++ src/day6.rs
@@ -1,6 +1,6 @@
 use std::mem::size_of_val;
 
-use ndarray::{Array1, Array2};
+use ndarray::{Array1, Array2, ArrayBase};
 
 use crate::utils::*;
 
@@ -8,17 +8,18 @@ const NEWBORN: usize = 8;
 const REFRESH: usize = 6;
 
 fn solve_log(input: &str, days: usize) -> usize {
-    let mut fishes = input
+    let fishes = input
         .trim()
         .split(',')
         .map(str::parse::<u8>)
         .map(Result::unwrap)
-        .fold(ArrayVec::from([0usize; NEWBORN + 1]), |mut acc, num| {
+        .fold(Array1::zeros(NEWBORN + 1), |mut acc, num| {
             acc[num as usize] += 1;
             acc
         });
 
     let steps = successors(Some(2), |x| Some(x * 2)).take_while(|&x| x <= days);
+    // TODO: can use nalgebra fixed size matrix/vector here
     let first = ndarray::array![
         [0, 1, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -31,21 +32,24 @@ fn solve_log(input: &str, days: usize) -> usize {
         [1, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
     let mats = {
-        let mut mats: Vec<Array2<usize>> = steps
+        let mut mats = steps
             .scan(first.clone(), |mat, _| {
                 *mat = mat.dot(mat);
                 Some(mat.clone())
             })
-            .collect_vec();
+            .collect::<ArrayVec<_, 32>>();
         mats.insert(0, first);
-        dbg!(mats)
+        mats
     };
 
     let mat = (0..(size_of_val(&days) * 8))
-        .filter(|bitn| days & (1 << bitn) != 0)
-        .map(|x| dbg!(x))
-        .fold(Array2::eye(NEWBORN + 1), |mat, i| mat.dot(&mats[i]));
-    mat.dot(&fishes.into_iter().collect::<Array1<_>>()).sum()
+        .zip(mats)
+        .filter(|(bitn, _)| days & (1 << bitn) != 0)
+        .map(|(_, mat)| mat)
+        .fold1(|a, b| a.dot(&b))
+        .unwrap();
+
+    mat.dot(&fishes).sum()
 }
 fn solve(input: &str, days: usize) -> usize {
     let mut fishes = input
