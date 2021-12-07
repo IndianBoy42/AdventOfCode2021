5b33dc5955b01792c44722b3124f469ec60dfa0d
Anshuman <amedhi@connect.ust.hk>
2021-12-07 15:26:09 +0800

    bench

--- benches/alldays.rs
+++ benches/alldays.rs
@@ -5,13 +5,12 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 macro_rules! bench_day {
     ($c:ident, $n:ident, $i:tt) => {{
-        let fname = concat!("input", $i, ".txt");
-        let input = utils::read_input(fname).unwrap();
+        let input = include_str!(concat!("../", "input", $i, ".txt"));
         $c.bench_function(concat!("day", $i, "p1"), |b| {
-            b.iter(|| $n::part1(black_box(&input)))
+            b.iter(|| $n::part1(black_box(input)))
         });
         $c.bench_function(concat!("day", $i, "p2"), |b| {
-            b.iter(|| $n::part2(black_box(&input)))
+            b.iter(|| $n::part2(black_box(input)))
         });
         input
     }};
@@ -36,10 +35,30 @@ pub fn criterion_benchmark(crit: &mut Criterion) {
     let day2 = bench_day!(c, day2, 2);
     let day3 = bench_day!(c, day3, 3);
     let day4 = bench_day!(c, day4, 4);
+    let day5 = bench_day!(c, day5, 5);
+    let day6 = bench_day!(c, day6, 6);
+    let day7 = bench_day!(c, day7, 7);
     c.bench_function("alldays", |b| {
-        b.iter(|| bench_all_days!(day1, day2, day3, day4))
+        b.iter(|| bench_all_days!(day1, day2, day3, day4, day5, day6, day7))
     });
 }
 
+macro_rules! iai_day {
+    ($b:ident, $n:ident, $i:tt) => {
+        fn $b() -> usize {
+            let input = include_str!(concat!("../", "input", $i, ".txt"));
+            $n::part1(black_box(input));
+            $n::part2(black_box(input))
+        }
+    };
+}
+
+iai_day!(bench_day1, day1, 1);
+iai_day!(bench_day2, day2, 2);
+iai_day!(bench_day3, day3, 3);
+iai_day!(bench_day4, day4, 4);
+iai_day!(bench_day5, day5, 5);
+
 criterion_group!(benches, criterion_benchmark);
+// iai::main!(bench_day1, bench_day2, bench_day3, bench_day4, bench_day5);
 criterion_main!(benches);
