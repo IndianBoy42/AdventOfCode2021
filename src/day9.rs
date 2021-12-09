99a715b7db2483667909ee44ddba36f3d732692c
Anshuman <amedhi@connect.ust.hk>
2021-12-09 14:04:03 +0800

    clean up a lot

--- src/day9.rs
+++ src/day9.rs
@@ -1,43 +1,11 @@
 use crate::{
-    grid::{adj_neighbours, Grid2D},
+    grid::{self, adj_neighbours, Grid2D},
     searcher::BFSearcher,
     utils::*,
 };
 
-pub fn part1(input: &str) -> usize {
-    let map = input
-        .lines()
-        .enumerate()
-        .flat_map(|(r, line)| {
-            line.bytes()
-                .enumerate()
-                .map(move |(c, cha)| ((r, c), cha - b'0'))
-        })
-        .collect::<FMap<(usize, usize), u8>>();
-
-    map.iter()
-        .filter(|&(&(r, c), &num)| {
-            let a = map.get(&(r + 1, c)).copied().unwrap_or(10);
-            let b = r
-                .checked_sub(1)
-                .and_then(|r| map.get(&(r, c)))
-                .copied()
-                .unwrap_or(10);
-            let d = c
-                .checked_sub(1)
-                .and_then(|c| map.get(&(r, c)))
-                .copied()
-                .unwrap_or(10);
-            let c = map.get(&(r, c + 1)).copied().unwrap_or(10);
-
-            (num < a) && (num < b) && (num < c) && (num < d)
-        })
-        .map(|(_, &num)| 1 + num as usize)
-        .sum::<usize>()
-}
-
-pub fn part2(input: &str) -> usize {
-    let map = input
+fn parse(input: &str) -> grid::Grid2D<u8> {
+    input
         .lines()
         .enumerate()
         .flat_map(|(r, line)| {
@@ -46,27 +14,40 @@ pub fn part2(input: &str) -> usize {
                 .map(move |(c, cha)| ((r, c), cha - b'0'))
         })
         // .collect::<FMap<(usize, usize), _>>();
-        .collect::<Grid2D<_>>();
+        .collect::<Grid2D<_>>()
+}
 
-    let points = map
-        .iter()
-        .filter(|&((r, c), &num)| {
-            let a = map.get(&(r + 1, c)).copied().unwrap_or(10);
-            let b = r
-                .checked_sub(1)
-                .and_then(|r| map.get(&(r, c)))
-                .copied()
-                .unwrap_or(10);
-            let d = c
-                .checked_sub(1)
-                .and_then(|c| map.get(&(r, c)))
-                .copied()
-                .unwrap_or(10);
-            let c = map.get(&(r, c + 1)).copied().unwrap_or(10);
+fn minima(map: &Grid2D<u8>) -> impl Iterator<Item = ((usize, usize), &u8)> {
+    map.iter().filter(|&((r, c), &num)| {
+        let a = map.get(&(r + 1, c)).copied().unwrap_or(10);
+        let b = r
+            .checked_sub(1)
+            .and_then(|r| map.get(&(r, c)))
+            .copied()
+            .unwrap_or(10);
+        let d = c
+            .checked_sub(1)
+            .and_then(|c| map.get(&(r, c)))
+            .copied()
+            .unwrap_or(10);
+        let c = map.get(&(r, c + 1)).copied().unwrap_or(10);
 
-            (num < a) && (num < b) && (num < c) && (num < d)
-        })
-        .collect_vec();
+        (num < a) && (num < b) && (num < c) && (num < d)
+    })
+}
+
+pub fn part1(input: &str) -> usize {
+    let map = parse(input);
+
+    minima(&map)
+        .map(|(_, &num)| 1 + num as usize)
+        .sum::<usize>()
+}
+
+pub fn part2(input: &str) -> usize {
+    let map = parse(input);
+
+    let points = minima(&map).collect_vec();
 
     points
         .into_iter()
